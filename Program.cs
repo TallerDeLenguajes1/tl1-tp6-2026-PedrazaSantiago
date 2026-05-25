86e21579ec9f1e1ecc7ccebcb219f18aa8f0dbf4
santiago <tpedraza17@gmail.com>
2026-05-25 19:05:56 -0300

    calculadoraV1 terminado

--- Program.cs
+++ Program.cs
@@ -22,3 +22,65 @@ else
 {
     Console.WriteLine("No es un numero valido");
 }
+
+char continuar;
+double num1;
+double num2;
+
+do
+{
+Console.WriteLine("Seleccione una opcion: ");
+Console.WriteLine("1. Sumar");
+Console.WriteLine("2. Restar");
+Console.WriteLine("3. Multiplicar");
+Console.WriteLine("4. Dividir");
+string opcion = Console.ReadLine();
+if (opcion != "1" && opcion != "2" && opcion != "3" && opcion != "4")
+{
+    Console.WriteLine("\nError: Opción no válida del menú.");
+}
+else
+{
+    Console.WriteLine("\nIngrese el primer numero: ");
+    while (!double.TryParse(Console.ReadLine(), out num1))
+    {
+    Console.WriteLine("Error, ingrese un numero: ");
+    }
+    Console.WriteLine("\nIngrese el segundo numero: ");
+    while (!double.TryParse(Console.ReadLine(), out num2))
+    {
+    Console.WriteLine("Error, ingrese un numero: ");
+    }
+    double resultado = 0;
+
+    switch (opcion)
+    {
+    case "1":
+    resultado = num1 + num2;
+    Console.WriteLine(resultado);
+    break;
+    case "2":
+    resultado = num1 - num2;
+    Console.WriteLine(resultado);
+    break;
+    case "3":
+    resultado = num1 * num2;
+    Console.WriteLine(resultado);
+    break;
+    case "4":
+    if (num2 != 0)
+    {
+        resultado = num1 / num2;
+        Console.WriteLine(resultado);
+    }
+    else
+    {
+        Console.WriteLine("No se puede dividir por cero");
+    }
+    break;
+    }
+    }
+
+    Console.WriteLine("\n¿Desea realizar otro calculo? (s/n): ");
+    continuar = char.ToLower(Console.ReadKey().KeyChar);
+    } while (continuar == 's');
