2ffa00912cd36910bda9b85acc71dad6c5d6cbca
santiago <tpedraza17@gmail.com>
2026-05-25 18:23:51 -0300

    Ejercicio1 completado

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿Console.WriteLine("Hello, World!");
+int a;
+int b;
+a=10;
+b=a;
+Console.WriteLine("Valor de a:"+a);
+Console.WriteLine("Valor de b:"+b);
+
+int invertido=0;
+string num = Console.ReadLine();
+if (int.TryParse(num, out int numero))
+{
+    while(numero > 0)
+    {
+        int ultDig = numero % 10;
+        invertido = (invertido*10) + ultDig;
+        numero = numero / 10;  
+    }
+    Console.WriteLine(invertido);
+}
+else
+{
+    Console.WriteLine("No es un numero valido");
+}
